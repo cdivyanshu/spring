208917bdf67d8e940a944e3dc645a36d085e9437
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2025-05-02 10:06:41 +0530

    Delete src/test/java/AppApplicationTests.java

--- src/test/java/AppApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.svn.app;
-
-@SpringBootApplication
-public class AppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
-    }
-}
